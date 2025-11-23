56bcf34b8685e27215ddcd24c3ae51be564f868d
Prashanth <Prashanthashok143@gmail.com>
2025-11-23 20:42:25 +0530

    added controllers and routes

--- server-side/src/server.ts
+++ server-side/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { router } from "./routes/tinyLinkRoutes"
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/dbConfig';
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/companies",router);
+app.use("/api/tinyLink",router);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
