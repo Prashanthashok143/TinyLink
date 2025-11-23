fa1580cb985ac0a937345cb2704fe5621bab01f9
Prashanth <Prashanthashok143@gmail.com>
2025-11-23 23:05:30 +0530

    links table and getCall updation

--- server-side/src/config/dbConfig.ts
+++ server-side/src/config/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/frontlines');
+        const connect = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/tinyLink');
         console.log(`MongoDB Connected: ${connect.connection.host}`);
     }catch (error){
         console.error(`Error: ${error}`);
