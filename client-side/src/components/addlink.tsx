77606f4e185c6b8d50368f247ba2edf42e69862d
Prashanth <Prashanthashok143@gmail.com>
2025-11-23 23:08:02 +0530

    linksTable , getCall updation

--- client-side/src/components/addlink.tsx
+++ client-side/src/components/addlink.tsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
+import { useCreateLinks } from "../api/tinyLinkService";
 
 interface AddLinkProps {
   model: boolean;
   setModel: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const AddLink: React.FC<AddLinkProps> = ({ model, setModel }) => {
-    const [codeData,setCodeData] = useState({
-        shortCode:"",
+const initialData={
+          code:"",
         targetURL:""
-    })
-    const handleChane=(e:React.ChangeEvent<HTMLInputElement>)=>{
+}
+const AddLink: React.FC<AddLinkProps> = ({ model, setModel }) => {
+  const {mutateAsync:createLinks} = useCreateLinks()
+    const [codeData,setCodeData] = useState(initialData)
+    const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
         const {name,value} = e.target;
         setCodeData({...codeData,[name]:value});
         console.log(e)
     }
+    console.log("data",codeData)
+    const handlePostLinks = async() => {
+      const {code, targetURL}=codeData;
+      if(!code)  return alert("pls fill the code")
+      if(!targetURL) return alert("pls fill the target")
+     await createLinks(codeData);
+    alert("Successsssssss")
+     setCodeData(initialData)
+    }
+    
   return (
     <>
       {model && (
@@ -32,20 +45,25 @@ const AddLink: React.FC<AddLinkProps> = ({ model, setModel }) => {
             <label className="Short-code-label">Short Code</label>
             <input
               type="text"
-              name="shortCode"
+              name="code"
+              value={codeData.code}
               placeholder="Enter Short Code"
               className="border-2 border-gray-300 rounded-md p-2 mt-4 w-full"
-              onChange={handleChane}
+              onChange={handleChange}
               />
               <label className="target-url-label">Target URL</label>
             <input
               type="text"
+              value={codeData.targetURL}
               placeholder="https://example.com"
               name="targetURL"
               className="border-2 border-gray-300 rounded-md p-2 mt-4 w-full"
-              onChange={handleChane}
+              onChange={handleChange}
               />
-            <button className="bg-black w-full text-white rounded-lg p-3 mt-2 font-bold cursor-pointer">Add Link</button>
+            <button className="bg-black w-full text-white rounded-lg p-3 mt-2 font-bold
+             cursor-pointer"
+             onClick={handlePostLinks}
+            >Add Link</button>
           </div>
         </div>
       )}
--- client-side/src/components/dashboard.tsx
+++ client-side/src/components/dashboard.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 import AddLink from "./addlink";
+import LinksTable from "./linksTable";
 
 const DashBoard = () => {
-  const [searchTerm , setSearchTerm] = useState("");
   const [model,setModel] = useState(false);
   return (
     <div className="bg-gray-50 min-h-screen">
@@ -10,13 +10,6 @@ const DashBoard = () => {
   <div className="text-3xl font-bold">TinyLink Dashboard</div>
 
   <div className="flex items-center gap-2">
-    <input
-      type="search"
-      value={searchTerm}
-      onChange={(e)=> setSearchTerm(e.target.value)}
-      placeholder="Search your links..."
-      className="border-2 border-gray-300 rounded-md p-2 mt-4 w-full"
-    />
     <button className="bg-black text-white
      rounded-md px-4 py-2 mt-4 shrink-0 
      cursor-pointer hover:bg-gray-800"
@@ -25,8 +18,11 @@ const DashBoard = () => {
     </button>
   </div>
 </div>
-<div className="flex justify-center">
+<div className="absolute right-0 pr-3 z-10">
   <AddLink model={model} setModel={setModel}/>
+</div>
+<div className="w-[75%] flex p-2">
+  <LinksTable/>
 </div>
 </div>
 
